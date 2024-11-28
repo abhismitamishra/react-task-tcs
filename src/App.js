3726328d202fbb80b220431f574ecabbf301ea4d
Abhismita <64856371+abhismitamishra@users.noreply.github.com>
2024-11-28 22:16:46 +0530

    Modify code and delete example.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import UserList from './Components/UserList'
-import UserListNew from './Components/UserListNew'
+
 
 const App = () => {
   return (
     <div>
 
-      <UserListNew/>
+      <UserList/>
     </div>
   )
 }
